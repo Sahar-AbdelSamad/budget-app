be67a762b309b545058e725bc0c0bab0ee44265f
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-23 12:25:55 +0300

    Add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  def home
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fullname, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fullname, :email, :password, :current_password) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+	devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "application#home"
 end
