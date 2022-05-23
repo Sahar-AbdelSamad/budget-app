80ae6499de3008e835aaec0a1d0555aa35fa47c6
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-23 18:57:14 +0300

    Add associations and validations

--- app/models/categorie.rb
+++ app/models/categorie.rb
@@ -1,2 +1,4 @@
 class Categorie < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :categories
+  validates :fullname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
