7c605c3734f48c3385209f092e12da6316a632c1
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-23 18:32:42 +0300

    Create categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,13 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 	devise_scope :user do
