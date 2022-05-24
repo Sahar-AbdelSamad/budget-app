014619c3e172614bc165c03ac88a1f8718eec7ca
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-24 11:17:30 +0300

    Add splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
+  root to: 'splash#index'
   devise_for :users
-  resources :categories, except: [:update, :edit, :destroy]
+  resources :categories, except: [:show, :update, :edit, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 	devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
   # Defines the root path route ("/")
-  root "categories#index"
 end
