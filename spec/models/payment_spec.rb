4b6f6b40051503982ff42be6b31392df03e4d569
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-26 01:41:18 +0300

    Add tests for payment model

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(fullname: 'sahar', email: 'sah@ar', password: '123123')
+    @user.save
+  end
+
+  subject do
+    Payment.new(author_id: @user.id, name: 'Rent', amount: 100)
+  end
+  before { subject.save }
+
+  describe 'validate data' do
+    it 'it should have a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have an amount' do
+      subject.amount = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'the amount should be numerical' do
+      subject.amount = 'a'
+      expect(subject).to_not be_valid
+    end
+  end
 end
